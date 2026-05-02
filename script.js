2ed15ef96898001f8f5cbbb31b7ea2bad48d7460
Shivam Maurya <mauryashivam2233@gmail.com>
2026-05-02 12:38:55 +0530

    password chanege '

--- script.js
+++ script.js
@@ -791,7 +791,7 @@ check_box.addEventListener('change', function(){
 
 //login user
 let user_name="nsctim@123"
-let password=052026;
+let password=123123;
 let login_name=document.getElementById("user");
 function login_usar(){
     if(pass.value==password && login_name.value==user_name){
